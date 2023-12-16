efeb74dff3476303a1ec9e4a4b81764885744be8
johnbarsigo <john.barsigo@student.moringaschool.com>
2023-12-16 14:55:04 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet( distance ){
+  if ( distance <=400 ){
+    return 'This one is on me!'
+  }
+  else if ( distance >2000 && distance < 2500 ){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if ( distance > 2500){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+//scuberGreetingForFeet(199);
+
+function ternaryCheckCity( destination ){
+  if ( destination == 'NYC'){
+    return 'Ok, sounds good.';
+  }
+  else if ( destination !== 'NYC') {
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+//console.log (ternaryCheckCity('NYC'));
+
+function switchOnCharmFromTip( tip ){
+  if (tip == 'generous'){
+    return 'Thank you so much.';
+  }
+  else if (tip == 'not as generous'){
+    return 'Thank you.';
+  }
+  else {
+    return 'Bye.';
+  }
 }
+
+//console.log( switchOnCharmFromTip('thanks for everything'));
